f1b75854893cff061468476dd8aa5a41a88938fe
PeiYu <b20321115@gmail.com>
2021-04-23 17:59:49 +0800

    cart page and image lazy load

--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,64 @@
-const Cart =() => {
-    return (
-      <div>
-        <h2>Cart</h2>
-        
-      </div>
-    )
+import styles from '../styles/page/Cart.module.sass'
+import { useState, useEffect } from 'react';
+import Image from 'next/image'
+
+const Cart = () => {
+  const [cartList, setCartList] = useState([])
+  const initData = () => {
+    setCartList([
+      {
+        id: 1,
+        name: 'product1',
+        price: 20,
+        img: 'https://picsum.photos/id/1/250/150'
+      },
+      {
+        id: 2,
+        name: 'product2',
+        price: 50,
+        img: 'https://picsum.photos/id/2/250/150'
+      },
+      {
+        id: 3,
+        name: 'product3',
+        price: 20,
+        img: 'https://picsum.photos/id/3/250/150'
+      },
+      {
+        id: 4,
+        name: 'product4',
+        price: 100,
+        img: 'https://picsum.photos/id/4/250/150'
+      }
+    ])
   }
-  
-  export default Cart
+  const sumPrice = () => {
+    return cartList.reduce((a, b) => a + (b.price || 0), 0);
+  }
+
+  useEffect(() => {
+    initData()
+  }, [])
+
+  return (
+    <div className={styles.cartPage}>
+      <h2 className={styles.cartTitle}>Cart</h2>
+      <div className={styles.cartItemWrapper}>
+        {cartList.map((item, index) => (
+          <div className={styles.cartItem} key={item.id}>
+            <div className={styles.itemImage}>
+              <Image src={item.img} layout="intrinsic" width={250} height={150} lazy></Image>
+            </div>
+            <div className={styles.itemContent}>
+              <div className={styles.itemTitle}>{item.name}</div>
+            </div>
+            <div className={styles.itemPrice}>$ {item.price}</div>
+          </div>
+        ))}
+        <div className={styles.cartTotal}>Total $ {sumPrice()}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
--- pages/components/PdCard.js
+++ pages/components/PdCard.js
@@ -1,20 +1,24 @@
 import { Card, CardMedia, CardContent } from '@material-ui/core';
 import styles from '../../styles/components/PdCard.module.sass'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const PdCard = (props) => {
     const handleImageError = (e) => {
-        e.target.onerror = null;
-        // e.target.style.display = 'none'
-        e.target.src = "https://dummyimage.com/250x150/bababa/ffffff&text=+++IMAGE+NOT+FOUND"
+        console.log(e)
+        // e.target.onerror = null;
+        // // e.target.style.display = 'none'
+        // e.target.src = "https://dummyimage.com/250x150/bababa/ffffff&text=+++IMAGE+NOT+FOUND"
     }
     return (
         <Card className={styles.pdCard}>
-            <CardMedia
+            <LazyLoadImage className={styles.pdCardMedia} src={props.url} width="100%" effect="blur" placeholderSrc={props.url}/>
+            {/* <CardMedia
                 component="img"
                 className={styles.pdCardMedia}
                 image={props.url}
                 onError={handleImageError}
-            />
+            /> */}
             <CardContent classes={{
                 root: styles.cardContent
             }}>
